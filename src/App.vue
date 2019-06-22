a22c1438cb6d2217fbc4be9c80978eafafd592e5
llj <912091370@qq.com>
2019-06-22 22:03:35 +0800

    加载vue脚手架，修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+      </el-row>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      formData:{
+        name:'',
+        bio:''
+      }
+    }
+  },
+  crreated(){
+    this.handleSubmit () 
+  },
+  methods: {
+    handleSubmit () {
+      const id=this.$route.params.id
+      axios({
+        method: 'GET',
+        url:`http//:localhost:3000/${id}`
+      }).then(res=>{
+        this.formData=res.data
+      })
     }
   }
 }
+</script>
+
+
+<style lang="less">
 </style>
